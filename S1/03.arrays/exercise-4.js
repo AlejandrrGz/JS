4007077e74ac3dfb56bfc8306d59994a909327a6
AlejandrrGz <144597526+AlejandrrGz@users.noreply.github.com>
2023-11-12 22:07:38 +0100

    Add files via upload

---
+++ S1/03.arrays/exercise-4.js
@@ -0,0 +1,21 @@
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty");
+rickAndMortyCharacters.push("Summer");
+
+// let lastCharacter = (rickAndMortyCharacters.length);
+
+const last = rickAndMortyCharacters[rickAndMortyCharacters.length-1];
+console.log(last);
+
+// const foundFirst = rickAndMortyCharacters.find((element) == rickAndMortyCharacters[0]);
+
+// console.log(foundFirst);
+
+
+
+// const array1 = [5, 12, 8, 130, 44];
+
+// const found = array1.find((element) => element > 10);
+
+// console.log(found);
---
+++ S1/03.arrays/exercise-5.js
@@ -0,0 +1,9 @@
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+RickAndMortyCharacters.pop();
+//console.log(RickAndMortyCharacters[4], RickAndMortyCharacters[0])
+
+console.log(RickAndMortyCharacters[0]);
+
+const last = RickAndMortyCharacters[RickAndMortyCharacters.length-1];
+console.log(last);
+
